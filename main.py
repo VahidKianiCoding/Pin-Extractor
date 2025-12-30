46f1c32dc1552686902a3ca321587dd3746ba44d
VahidKianiCoding <165814462+VahidKianiCoding@users.noreply.github.com>
2025-12-30 12:53:24 +0330

    Fix secret code generation logic in main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def pin_extractor(poem):
 
         if len(words) > line_index:
             secret_code += str(len(words[line_index]))
+        else:
+            secret_code += "0"
 
     return secret_code
 
