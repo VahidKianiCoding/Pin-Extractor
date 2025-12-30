4c872c9a1a0a6d9c72e41f169039f36669f6a505
VahidKianiCoding <165814462+VahidKianiCoding@users.noreply.github.com>
2025-12-30 12:35:59 +0330

    Print each line of the poem in pin_extractor
    Added a loop to print each line of the poem.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def pin_extractor(poem):
     secret_code = ''
     lines = poem.split('\n')
+    for line in lines:
+        print(line)
 
 poem = "Stars and the moon\nshine in the sky\nwhite and bright\nuntil the end of the night"
 
